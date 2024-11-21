9c14b7ccc1a1a89f84a8adbb74c134b032815a6d
joshuaenoch <joshuaechow@gmail.com>
2024-11-20 17:40:53 -0800

    user information retrieved in profile

--- 481-final-project-app/src/components/Profile.jsx
+++ 481-final-project-app/src/components/Profile.jsx
@@ -16,13 +16,21 @@ export default function Profile() {
       if (obj.data) {
         setUserInfo(obj.data)
         console.log(userInfo)
+        /*
+        userInfo is structured like this:
+        accountId: "f4a7cb12-5fa6-4f7b-a788-fb6fa64491d5"
+        bio: "UW alumni with a background in computer science and business administration. 4+ experience working in the tech and finance industries. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        firstName: "Alice"
+        lastName: "Doe"
+        profilePicture: "profile_pictures/f4a7cb12-5fa6-4f7b-a788-fb6fa64491d5/Alice.jpg"
+        */
       } else if (obj.error) {
         console.log("There was an error with the Profile page")
       }
     }
 
     fetchData()
-  }, []);
+  }, [id, userInfo]);
 
   return (
     <div style={{marginTop: '80px'}}>
@@ -30,7 +38,7 @@ export default function Profile() {
       <img className="profile-picture" alt="pfp"/>
       <div className="profile-header">
         <div className="profile-name">
-          {id}
+          Tushar
         </div>
         {/*<button className="connect-button">Connect</button>*/}
       </div>
