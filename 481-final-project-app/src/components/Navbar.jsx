5b16c68d25739f3fe6d2c1f97add1e98cc3b4239
joshuaenoch <joshuaechow@gmail.com>
2024-11-26 14:47:28 -0800

    fixed navbar logic error

--- 481-final-project-app/src/components/Navbar.jsx
+++ 481-final-project-app/src/components/Navbar.jsx
@@ -8,7 +8,9 @@ export default function Navbar() {
   useEffect(() => {
     const auth = new SupabaseAuthentication();
     const fetchUser = async () => {
-      setLoggedIn(await auth.retrieveUser());
+      const user = await auth.retrieveUser()
+      .then((user) => user ? setLoggedIn(true) : setLoggedIn(false));
+      console.log(loggedIn)
     };
 
     fetchUser();
@@ -16,11 +18,11 @@ export default function Navbar() {
 
   return (
     <div className="navbar">
-      <Link to={loggedIn ? '/' : `/home`}><img id="logo"/></Link>
+      <Link to={loggedIn ? '/home' : `/`}><img id="logo"/></Link>
       <div class="navbar-right">
-        <a><Link to={loggedIn ? '/' : `/home`}>Explore</Link></a>
-        <a><Link to={loggedIn ? '/' : "/connections"}>Connections</Link></a>
-        <a><Link to={loggedIn ? '/' : "/profile"}><img id="profile-icon"/></Link></a>
+        <a><Link to={loggedIn ? '/home' : `/`}>Explore</Link></a>
+        <a><Link to={loggedIn ? '/connections' : "/"}>Connections</Link></a>
+        <a><Link to={loggedIn ? '/profile' : "/"}><img id="profile-icon"/></Link></a>
       </div>
     </div>
   )
