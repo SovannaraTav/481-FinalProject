e040a1efba885a9da94cb0412a10f5d43545d3b0
Sovannara Tav <129139304+SovannaraTav@users.noreply.github.com>
2024-12-02 21:43:11 -0800

    Implemented code to display profile pictures in the profile cards of home page

--- 481-final-project-app/src/components/ProfileCard.jsx
+++ 481-final-project-app/src/components/ProfileCard.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import defaultPic from "../assets/default.jpg"
+import React, {useEffect, useState}  from 'react';
+import SupabaseStorage from '../classes/SupabaseStorage';
+import defaultPic from '../assets/default.jpg';
 
 export default function ProfileCard({name, picture, role=null}) {
+  const storage = new SupabaseStorage("profile_pictures");
+  const [profilePicture, setProfilePicture] = useState(defaultPic);
+
+  useEffect(() => {
+    const fetchSenderProfilePicture = async () => {
+      if (picture !== "") {
+        const profilePictureUrl = await storage
+          .generatePublicProfilePictureUrl(picture);
+        setProfilePicture(profilePictureUrl.data.publicUrl);
+      }
+    }
+
+    fetchSenderProfilePicture();
+  }, []);
+
   return (
     <div className="card">
-      <img className="card-image" src={defaultPic}></img>
+      <img className="card-image" src={profilePicture}></img>
       <div className="card-description">{name}</div>
     </div>
   )
