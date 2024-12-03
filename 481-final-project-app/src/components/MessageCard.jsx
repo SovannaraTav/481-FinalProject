699c910ac66a1ac64e975ec2e520fadfaeab73bc
Sovannara Tav <129139304+SovannaraTav@users.noreply.github.com>
2024-12-02 20:43:39 -0800

    Implemented code to display profile pictures in the messaging service inbox

--- 481-final-project-app/src/components/MessageCard.jsx
+++ 481-final-project-app/src/components/MessageCard.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import SupabaseDatabase from '../classes/SupabaseDatabase';
+import SupabaseStorage from '../classes/SupabaseStorage';
 import defaultPic from '../assets/default.jpg'
 
 export default function MessageCard({ message }) {
+  const db = new SupabaseDatabase();
+  const storage = new SupabaseStorage("profile_pictures");
+  const [profilePicture, setProfilePicture] = useState(defaultPic);
+
+  useEffect(() => {
+    const fetchSenderProfilePicture = async () => {
+      const obj = await db
+        .readRecordFromTable("accounts", "accountId", `${message.senderId}`);
+      if (obj.data) {
+        if (obj.data[0].profilePicture !== "") {
+          const profilePictureUrl = await storage
+            .generatePublicProfilePictureUrl(obj.data[0].profilePicture);
+          setProfilePicture(profilePictureUrl.data.publicUrl);
+        }
+      }
+    }
+
+    fetchSenderProfilePicture();
+  }, []);
+
   return (
     <div className="message-grid">
       <div className="horizontal">
-        <img id="profile-icon" alt="pfp" src={defaultPic}></img>
+        <img id="profile-icon" alt="pfp" src={profilePicture}></img>
         <div id="grid-item2">
           {message.accounts.firstName} {message.accounts.lastName}
         </div>
