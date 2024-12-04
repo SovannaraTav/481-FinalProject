797aec729dd5cf0c9c9f9afc356f3cd8e1366228
joshuaenoch <joshuaechow@gmail.com>
2024-12-04 02:02:44 -0800

    message area css fixed but has a weird bug of scrolling to the bottom of the page

--- 481-final-project-app/src/components/MessageBox.jsx
+++ 481-final-project-app/src/components/MessageBox.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import SupabaseRealtime from '../classes/SupabaseRealtime'
 import Message from '../classes/Message';
 import SupabaseAuthentication from '../classes/SupabaseAuthentication';
@@ -8,32 +8,38 @@ export default function MessageBox({ id, thread }) {
   const messaging = new SupabaseRealtime();
   const [messageContent, setMessageContent] = useState("");
   const [user, setUser] = useState(null);
-  const[talkingTo, setTalkingTo] = useState(null);
-  // TODO: make it so that it actually listens for new messages from the database
+  const [talkingTo, setTalkingTo] = useState(null);
   const [updatedThread, setUpdatedThread] = useState(thread);
 
-  useEffect(()=>{
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
     const auth = new SupabaseAuthentication();
     const getUser = async () => {
       setUser(await auth.retrieveUser());
-    }
-
-    getUser()
-  }, [])
+    };
+    getUser();
+  }, []);
 
   useEffect(() => {
     setUpdatedThread(thread);
 
     const db = new SupabaseDatabase();
     const getTalkingTo = async () => {
-      const obj2 = await db.readRecordFromTable("accounts", "accountId", id)
+      const obj2 = await db.readRecordFromTable("accounts", "accountId", id);
       if (obj2.data) {
         setTalkingTo(obj2.data[0]);
       }
-    }
+    };
+    getTalkingTo();
+  }, [id]);
 
-    getTalkingTo()
-  }, [id])
+  useEffect(() => {
+    // Scroll to the bottom whenever the message thread is updated
+    if (messagesEndRef.current) {
+      messagesEndRef.current.scrollIntoView();
+    }
+  }, [updatedThread]); // Trigger when updatedThread changes
 
   const handleSentMessage = (e) => {
     e.preventDefault();
@@ -54,71 +60,109 @@ export default function MessageBox({ id, thread }) {
         setMessageContent("");
       });
     }
+  };
+
+  if (!user || !talkingTo) {
+    return <div></div>;
   }
 
-  if(!user || !talkingTo){
-    return <div>Start the conversation</div>
-  }
   return (
-    <div>
+    <div style={{
+      display: "flex",
+      flexDirection: "column",
+      height: "100%",
+      maxHeight: "500px",
+      backgroundColor: "#fff",
+      position: "relative",
+      overflowX: "hidden"
+    }}>
       <div style={{
+        position: "sticky",
+        top: "0",
+        backgroundColor: "#fff",
+        fontSize: "20px",
+        zIndex: "10",
+        textAlign: "center",
         padding: "5px 10px",
         marginBottom: "10px",
         borderBottom: "1px solid lightgray",
-        fontSize: "20px",
         color: "#4b207e",
       }}>
         {talkingTo.firstName} {talkingTo.lastName}
       </div>
-      <div>
-        {updatedThread.map(msg => {
-          return <div
-            style={{
-              backgroundColor: msg.senderId === user.id ? "#98dde2" : "#dad7e5",
-              color: "white",
-              padding: "10px",
-              marginBottom: "5px",
-              borderRadius: "5px",
-            }}
-          >
-            <div style={{position: 'sticky',
-              top: '0',
-              left: '0'}}
+      <div style={{
+        display: "flex",
+        flexDirection: "column",
+        flexGrow: "1",
+        padding: "10px",
+        overflowY: "auto",
+        marginBottom: "50px",
+        scrollbarWidth: "none"
+      }}>
+        {updatedThread.map((msg, idx) => {
+          return (
+            <div
+              key={idx}
+              style={{
+                backgroundColor: msg.senderId === user.id ? "#0078FE" : "#989898",
+                color: "white",
+                padding: "10px",
+                marginBottom: "5px",
+                borderRadius: "5px",
+              }}
             >
-              {msg.content}
+              <div>{msg.content}</div>
+              <div style={{
+                fontSize: "12px",
+                color: "#f6f6f6",
+              }}>
+                {msg.senderId === user.id ? <span>You</span> : <span>{talkingTo.firstName}</span>}
+                on {msg.dateTime}
+              </div>
             </div>
-            <div style={{
-              display: "block",
-              fontSize: "12px",
-              color: "#f6f6f6"
-            }}>
-              {msg.senderId === user.id ?
-              <span>You </span> :<span>{name} </span>}
-              on&nbsp;
-              {msg.dateTime}
-            </div>
-          </div>
+          )
         })}
+        <div ref={messagesEndRef} />  {/* This div will act as the target for scrolling */}
       </div>
-      <div>
+      <div style={{
+        position: "absolute",
+        bottom: "0",
+        left: "0",
+        width: "100%",
+        padding: "10px",
+        backgroundColor: "#fff",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}>
         <input
           value={messageContent}
           placeholder="Enter message here to send"
           onChange={(e) => setMessageContent(e.target.value)}
           style={{
-            position: "sticky",
-            bottom: "0",
-            left: "0",
-            width: "98%",
-            height: "30px",
-            background: "#fff",
+            width: "85%",
+            padding: "8px",
             border: "1px solid lightgray",
             borderRadius: "5px",
-            marginTop: "150px",
-        }}
-        ></input>
-        <button onClick={handleSentMessage}>Send</button>
+          }}
+        />
+        <button
+          onClick={handleSentMessage}
+          style={{
+            padding: "0px",
+            backgroundColor: "#4b207e",
+            color: "white",
+            border: "none",
+            borderRadius: "5px",
+            maxWidth: "50px",
+            maxHeight: "30px",
+            margin: "0 20px 0 10px",
+            fontSize: "15px"
+          }}
+        >
+          Send
+        </button>
       </div>
     </div>
-  )
+  );
 }
